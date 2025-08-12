e09e4bab8c22277357b62570d5b76c3c120ad320
Sai Samarth S Budihal <157210705+Knuckle09@users.noreply.github.com>
2025-08-12 23:35:17 +0530

    31492

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ from django.urls import path, inlude
 urlpatterns = [
     path('',views.index,name='home'),
     path('admin/',admin.site.urls),
+    path('',views.home,name='home'),
     path('',include('home.urls'));
 ]
