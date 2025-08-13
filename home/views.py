7766f0550b23e8819a52e04992bc87fa3cafac9f
Sai Samarth S Budihal <157210705+Knuckle09@users.noreply.github.com>
2025-08-13 21:49:37 +0530

    31493

--- home/views.py
+++ home/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render
-from django.conf import settings
 
+def contact_view(request):
+    contact_info={
+        'phone':'+91 9876543210',
+        'email':'contact@restaurant.com',
+        'address':'123 Food Street, Bengaluru, Karnataka, India'
+    }
 
-def home_view(request):
-    restaurant_name=getattr(settings,"RESTAURANT_NAME","Restaurant")
-    return render(request,"home/index.html",{"restaurant_name":restaurant_name})
-
-def about_view(request):
-    restaurant_name=getattr(settings,"RESTAURANT_NAME","Restaurant")
-    description=(
-        "Welcome to {0}! We serve fresh, locally sourced food prepared with care. "
-        "Our mission is to provide a warm dining experiance for families and friends. "
-    ).format(restaurant_name)
-
-return render(request,"home/about.html",{
-    "restaurant_name":restaurant_name,
-    "description":description,
-})
+    return render(request,'contact.html',{'contact_info':contact_info})
