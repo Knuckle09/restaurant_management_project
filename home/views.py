194d2bb2cd2b42d24518d047388cfe8afb987f32
Sai Samarth S Budihal <157210705+Knuckle09@users.noreply.github.com>
2025-08-14 23:29:47 +0530

    31495

--- home/views.py
+++ home/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 
-def contact_view(request):
-    contact_info={
-        'phone':'+91 9876543210',
-        'email':'contact@restaurant.com',
-        'address':'123 Food Street, Bengaluru, Karnataka, India'
+def home_view(request):
+    context={
+
+        'restaurant_name':'Delicious Bites'
     }
 
-    return render(request,'contact.html',{'contact_info':contact_info})
+    return render(request,'home.html',context)
