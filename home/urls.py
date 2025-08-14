7b0e712e5b4bbe44f1914e8444d8920fe5d87512
Sai Samarth S Budihal <157210705+Knuckle09@users.noreply.github.com>
2025-08-14 23:31:00 +0530

    31495

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import home_view
 
 urlpatterns=[
     path('',views.homepage_view,name='homepage'),
+    path('',home_view,name='home')
 ]
